1dbcb7f678f9190fa07bd9f974895531fb9a49a3
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-10-10 17:37:54 +0800

    landtoni: extract-files: Remove blob fixups
    Here we should use fixed blobs from device specific trees.

--- extract-files.sh
+++ extract-files.sh
@@ -6,65 +6,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-function blob_fixup() {
-    case "${1}" in
-        vendor/bin/mm-qcamera-daemon)
-            sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
-            "${PATCHELF}" --print-needed "${2}"|grep "libshims_c_camera.so">/dev/null
-            if [ $? -ne 0 ]; then
-                "${PATCHELF}" --add-needed "libshims_c_camera.so" "${2}"
-            fi
-            ;;
-        vendor/lib/libmmsw_platform.so|libmmsw_detail_enhancement.so)
-            "${PATCHELF}" --remove-needed "libbinder.so" "${2}"
-            sed -i 's|libgui.so|libwui.so|g' "${2}"
-            ;;
-        vendor/lib/libmmcamera2_sensor_modules.so)
-            sed -i 's|/system/etc/camera/|/vendor/etc/camera/|g' "${2}"
-            sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
-            ;;
-        vendor/lib/libmmcamera_tintless_bg_pca_algo.so \
-        |vendor/lib/libmmcamera_pdafcamif.so \
-        |vendor/lib/libmmcamera2_dcrf.so \
-        |vendor/lib/libmmcamera_imglib.so \
-        |vendor/lib/libmmcamera_dbg.so \
-        |vendor/lib/libmmcamera2_stats_algorithm.so \
-        |vendor/lib/libmmcamera2_mct.so \
-        |vendor/lib/libmmcamera_tuning.so \
-        |vendor/lib/libmmcamera_tintless_algo.so \
-        |vendor/lib/libmmcamera2_iface_modules.so \
-        |vendor/lib/libmmcamera2_q3a_core.so \
-        |vendor/lib/libmmcamera2_pproc_modules.so \
-        |vendor/lib/libmmcamera2_imglib_modules.so \
-        |vendor/lib/libmmcamera2_cpp_module.so \
-        |vendor/lib/libmmcamera_pdaf.so)
-            sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
-            ;;
-        vendor/lib/libmmcamera2_stats_modules.so)
-            sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
-            sed -i 's|libgui.so|libwui.so|g' "${2}"
-            "${PATCHELF}" --replace-needed "libandroid.so" "libshims_android.so" "${2}"
-            ;;
-        vendor/bin/gx_fpcmd|vendor/bin/gx_fpd)
-            patchelf --remove-needed "libbacktrace.so" "${2}"
-            patchelf --remove-needed "libunwind.so" "${2}"
-            "${PATCHELF}" --print-needed "${2}"|grep "liblog.so">/dev/null
-            if [ $? -ne 0 ]; then
-                patchelf --add-needed "liblog.so" "${2}"
-            fi
-            ;;
-        vendor/lib64/libfpservice.so)
-            "${PATCHELF}" --print-needed "${2}"|grep "libshims_binder.so">/dev/null
-            if [ $? -ne 0 ]; then
-                patchelf --add-needed "libshims_binder.so" "${2}"
-            fi
-            ;;
-        vendor/lib64/hw/fingerprint.goodix.so)
-            sed -i 's|libandroid_runtime.so|libshims_android.so\x00\x00|g' "${2}"
-            ;;
-    esac
-}
-
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
