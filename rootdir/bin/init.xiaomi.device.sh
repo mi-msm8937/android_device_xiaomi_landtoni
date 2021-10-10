9a88060d79a7ac7ea0e57267ba878aa475e5c9e9
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-10-10 16:44:16 +0800

    landtoni: Support detecting santoni device
    Co-authored-by: Prateek Chaubey <chaubeyprateek@gmail.com>

--- rootdir/bin/init.xiaomi.device.sh
+++ rootdir/bin/init.xiaomi.device.sh
@@ -1,9 +1,20 @@
 #!/vendor/bin/sh
 
-grep -E "S88537AC1|S88537EC1" /proc/cmdline
-if [ $? -eq 0 ]; then
-	setprop ro.vendor.fingerprint.supported 0
-else
+if grep S88537 /proc/cmdline ; then
+	# land
+	setprop ro.vendor.xiaomi.device land
+
+	# Fingerprint
+	if grep -E "S88537AC1|S88537EC1" /proc/cmdline ; then
+		setprop ro.vendor.fingerprint.supported 0
+	else
+		setprop ro.vendor.fingerprint.supported 1
+	fi
+elif grep S88536 /proc/cmdline ; then
+	# santoni
+	setprop ro.vendor.xiaomi.device santoni
+
+	# Fingerprint
 	setprop ro.vendor.fingerprint.supported 1
 fi
 
