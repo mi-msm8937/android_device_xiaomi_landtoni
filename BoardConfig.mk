mi-msm8937/android_device_xiaomi_santoni

land: Inherit from common tree
Change-Id: I9eb2ca59312c55d494cb18557689898d9cdf5f62

BoardConfig.mk
@@ -1,0 +1,19 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from common msm8937-common
+include device/xiaomi/msm8937-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/land
+
+# Kernel
+TARGET_KERNEL_CONFIG := mi8937_defconfig
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2017-04-01
+
+# Inherit from the proprietary version
+include vendor/xiaomi/land/BoardConfigVendor.mk

device.mk
@@ -1,0 +1,16 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from msm8937-common
+$(call inherit-product, device/xiaomi/msm8937-common/msm8937.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/xiaomi/land/land-vendor.mk)

extract-files.sh
@@ -1,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=land
+export DEVICE_COMMON=msm8937-common
+export VENDOR=xiaomi
+
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"

setup-makefiles.sh
@@ -1,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE=land
+export DEVICE_COMMON=msm8937-common
+export VENDOR=xiaomi
+
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"

mi-msm8937/android_device_xiaomi_landtoni

BoardConfig.mk
@@ -1,0 +1,19 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from common landtoni-common
+include device/xiaomi/landtoni-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/land
+
+# Kernel
+TARGET_KERNEL_CONFIG := mi8937_defconfig
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2017-04-01
+
+# Inherit from the proprietary version
+include vendor/xiaomi/land/BoardConfigVendor.mk

device.mk
@@ -1,0 +1,16 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from landtoni-common
+$(call inherit-product, device/xiaomi/landtoni-common/landtoni.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/xiaomi/land/land-vendor.mk)

extract-files.sh
@@ -1,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=land
+export DEVICE_SPECIFIED_COMMON=landtoni-common
+export VENDOR=xiaomi
+
+"./../../${VENDOR}/${DEVICE_SPECIFIED_COMMON}/extract-files.sh" "$@"

setup-makefiles.sh
@@ -1,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE=land
+export DEVICE_SPECIFIED_COMMON=landtoni-common
+export VENDOR=xiaomi
+
+"./../../${VENDOR}/${DEVICE_SPECIFIED_COMMON}/setup-makefiles.sh" "$@"